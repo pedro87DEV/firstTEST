d554a892784c5011d0391e950110ec650737f7e3
pedro87DEV <45098587+pedro87DEV@users.noreply.github.com>
2018-11-22 15:43:22 +0100

    Add files via upload

---
+++ write_blockchain.sh
@@ -0,0 +1,42 @@
+#! /bin/bash
+#########################################################################
+#
+# Script per scrittura messaggio sulla blockchain
+# Realizzato da Mattia Pedroncelli  
+#
+##########################################################################
+
+
+########################################################################## 
+# DEFINIZIONE VARIABILI UTILIZZATE NELLO SCRIPT
+# Parametrizzare le variabili 
+##########################################################################
+TIMESTAMP=`date +%Y%m%d-%H%M%S`
+DIR_FILE_TO_CONVERT='/home/smart/SVIL/PEDRO/file_to_convert'
+DIR_FILE_WRITE='/home/smart/SVIL/PEDRO/file_write_blockchain_done'
+
+#echo -n “Inserisci il nome file da convertire“
+#read FILE_TO_CONVERT 
+ 
+for i in $(ls $DIR_FILE_TO_CONVERT);
+
+  do
+
+  ##########################################################################
+  # HASHING
+  # CONVERSIONE FILE IN STRINGA FORMATO SHA256
+  # Viene generato in output un file contenente la stringa 
+  ##########################################################################
+
+   sha256sum $DIR_FILE_TO_CONVERT/$i >> file_sha_hashed'_'$TIMESTAMP.txt
+  
+
+  ##########################################################################
+  # Lancio dello script Python
+  ##########################################################################
+
+   python store-OP_RETURN.py 'PROVA'
+
+  done
+
+ 
